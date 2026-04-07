2c9ebd47fd3307568776a730b25bda607a5b1399
czp <celenzo.peuch@epitech.eu>
2026-04-07 03:52:14 +0200

    feat: add src/utils/strings/my_strdup.c

---
+++ src/utils/strings/my_strdup.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2026
+** strdup
+** File description:
+** recode
+*/
+
+#include "minishell.h"
+
+char *my_strdup(char *str)
+{
+    int i = 0;
+    int len = my_strlen(str);
+    char *dup = malloc(sizeof(char) * (len + 1));
+
+    for (i = 0; str[i] != '\0'; i++)
+        dup[i] = str[i];
+    dup[i] = '\0';
+    return dup;
+}
