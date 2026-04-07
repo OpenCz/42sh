f4bf76d158284074d5f280e62e6bc4e64edcee98
czp <celenzo.peuch@epitech.eu>
2026-04-07 03:52:14 +0200

    add src/utils/validation/my_str_is_alphanum.c

---
+++ src/utils/validation/my_str_is_alphanum.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2026
+** my_str_to_alphanum
+** File description:
+** num
+*/
+
+#include "minishell.h"
+
+int my_str_is_alphanum(char *key)
+{
+    int i = 0;
+
+    while (key[i] != '\0') {
+        if (!((key[i] >= '0' && key[i] <= '9') ||
+                (key[i] >= 'A' && key[i] <= 'Z') ||
+                (key[i] >= 'a' && key[i] <= 'z'))) {
+            return 1;
+        }
+        i++;
+    }
+    return 0;
+}
