1c927027fa8e3e14757df28a79c43bf66646a166
czp <celenzo.peuch@epitech.eu>
2026-04-07 03:52:13 +0200

    feat: add src/builtins/env/my_env.c

---
+++ src/builtins/env/my_env.c
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2026
+** my_env
+** File description:
+** my_env
+*/
+
+#include "minishell.h"
+
+int my_env(main_t *main_stock)
+{
+    for (env_t *tmp = main_stock->stock_env; tmp; tmp = tmp->next) {
+        my_putstr(tmp->key);
+        my_putstr("=");
+        if (tmp->value)
+            my_putstr(tmp->value);
+        my_putstr("\n");
+    }
+    return 0;
+}
+
+int builtin_env(main_t *main_stock, command_ctx_t *ctx)
+{
+    (void)ctx;
+    return my_env(main_stock);
+}
