ae9179766fdab4c25becabc031ec362455074a50
czp <celenzo.peuch@epitech.eu>
2026-04-07 03:52:13 +0200

    feat: add include/builtins/builtins.h

---
+++ include/builtins/builtins.h
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2026
+** minishell
+** File description:
+** builtins api
+*/
+
+#ifndef BUILTINS_H
+    #define BUILTINS_H
+
+    #include "core/types.h"
+
+typedef int (*builtin_handler_t)(main_t *main_stock, command_ctx_t *ctx);
+
+typedef struct builtin_command_s {
+    const char *name;
+    builtin_handler_t func;
+} builtin_command_t;
+
+int execute_builtin(main_t *main_stock, command_ctx_t *ctx);
+int builtin_env(main_t *main_stock, command_ctx_t *ctx);
+int builtin_setenv(main_t *main_stock, command_ctx_t *ctx);
+int builtin_unsetenv(main_t *main_stock, command_ctx_t *ctx);
+int builtin_cd(main_t *main_stock, command_ctx_t *ctx);
+
+int my_env(main_t *main_stock);
+int my_setenv(main_t *main_stock);
+int my_unsetenv(main_t *main_stock);
+int my_chdir(main_t *main_stock);
+
+#endif
