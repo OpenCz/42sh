5ee2b6b31a924f6b59033888a81289011c5d9d2f
czp <celenzo.peuch@epitech.eu>
2026-04-07 03:52:14 +0200

    add src/environment/query/get_path.c

---
+++ src/environment/query/get_path.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2026
+** get
+** File description:
+** path
+*/
+
+#include "minishell.h"
+
+char *get_path(env_t *stock_env)
+{
+    for (env_t *tmp = stock_env; tmp; tmp = tmp->next) {
+        if (my_strcmp(tmp->key, "PATH") == 0 && tmp->value != NULL)
+            return tmp->value;
+    }
+    return NULL;
+}
