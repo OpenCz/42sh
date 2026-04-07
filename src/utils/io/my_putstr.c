0085101fe836f91b010200b22294b68975586ca5
czp <celenzo.peuch@epitech.eu>
2026-04-07 03:52:14 +0200

    feat: add src/utils/io/my_putstr.c

---
+++ src/utils/io/my_putstr.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2026
+** my_pputstr
+** File description:
+** my_putstr
+*/
+
+#include "minishell.h"
+
+void my_putchar(char c, int sortie)
+{
+    write(sortie, &c, 1);
+}
+
+void my_putstr(char *str)
+{
+    for (int i = 0; str[i] != '\0'; i++) {
+        my_putchar(str[i], 1);
+    }
+}
+
+void my_putstrerror(char *str)
+{
+    for (int i = 0; str[i] != '\0'; i++) {
+        my_putchar(str[i], 2);
+    }
+}
