5f2c11f9bdd2f98feebc1a161902d9697ba5b8ea
czp <celenzo.peuch@epitech.eu>
2026-04-07 03:52:13 +0200

    feat: add include/minishell.h

---
+++ include/minishell.h
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2026
+** minishell
+** File description:
+** shell
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+
+#ifndef MINISHELL_H
+    #define MINISHELL_H
+
+    #include "core/types.h"
+    #include "core/core.h"
+    #include "utils/utils.h"
+    #include "environment/environment.h"
+    #include "parsing/parsing.h"
+    #include "builtins/builtins.h"
+    #include "execution/execution.h"
+    #include "memory/memory.h"
+
+#endif
