4013bbbda88a194fe99b72c96e86643c12bea1f9
czp <celenzo.peuch@epitech.eu>
2026-04-07 03:52:14 +0200

    feat: add src/execution/dispatch/execute_single_command.c

---
+++ src/execution/dispatch/execute_single_command.c
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2026
+** execute
+** File description:
+** single command
+*/
+
+#include "minishell.h"
+
+int execute_single_command(main_t *stock_main,
+    char *command, bool allow_builtin)
+{
+    command_ctx_t ctx;
+    int parse_status = parse_command_context(command, &ctx);
+    int builtin_status = -1;
+
+    if (parse_status == 1)
+        return 1;
+    if (parse_status == 2)
+        return 0;
+    if (allow_builtin) {
+        builtin_status = execute_builtin(stock_main, &ctx);
+        if (builtin_status != -1) {
+            clear_command_ctx(&ctx);
+            return builtin_status;
+        }
+    }
+    builtin_status = exec_any(stock_main, &ctx);
+    clear_command_ctx(&ctx);
+    return builtin_status;
+}
