d4ae770de5d98afcc16fa7cf02467a2220b45b5b
czp <celenzo.peuch@epitech.eu>
2026-04-07 03:52:14 +0200

    add src/execution/dispatch/execute_command.c

---
+++ src/execution/dispatch/execute_command.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2026
+** execute
+** File description:
+** command
+*/
+
+#include "minishell.h"
+
+static int execute_compound_command(main_t *stock_main, char *command)
+{
+    if (my_strstr(command, "|") != NULL)
+        return execute_pipeline(stock_main, command);
+    return execute_single_command(stock_main, command, true);
+}
+
+int execute_command(main_t *stock_main, char *command)
+{
+    char **commands = my_str_to_word_array(command, ";");
+    int last_status = 0;
+
+    if (!commands)
+        return 1;
+    for (int i = 0; commands[i] != NULL; i++)
+        last_status = execute_compound_command(stock_main, commands[i]);
+    free_array(commands);
+    return last_status;
+}
