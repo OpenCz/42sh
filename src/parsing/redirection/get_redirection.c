5c599605b4321367f57004ff82c4041009f90a5e
czp <celenzo.peuch@epitech.eu>
2026-04-07 03:52:14 +0200

    feat: add src/parsing/redirection/get_redirection.c

---
+++ src/parsing/redirection/get_redirection.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2026
+** redirection
+** File description:
+** redirection
+*/
+
+#include "minishell.h"
+
+char *get_redirection(const char *command)
+{
+    const char *redirs[] = { ">>", "<<", ">", "<" };
+    size_t n = sizeof(redirs) / sizeof(redirs[0]);
+
+    for (size_t i = 0; i < n; i++) {
+        if (my_strstr(command, redirs[i]) != NULL)
+            return (char *)redirs[i];
+    }
+    return NULL;
+}
