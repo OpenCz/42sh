e05630edb606948e33a49f722ff2a698cf939e44
czp <celenzo.peuch@epitech.eu>
2026-04-07 03:52:14 +0200

    add src/execution/external/run_fork.c

---
+++ src/execution/external/run_fork.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2026
+** run fork
+** File description:
+** run
+*/
+
+#include "minishell.h"
+
+int run_fork(main_t *main_stock, command_ctx_t *ctx, char *path, char **env)
+{
+    pid_t pid = fork();
+    int status = 0;
+
+    if (pid < 0) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    if (pid == 0) {
+        if (apply_redirection(ctx) != 0)
+            exit(1);
+        exit(child_exec(ctx, path, env));
+    } else {
+        waitpid(pid, &status, 0);
+        status = get_seg(status);
+    }
+    return status;
+}
