6c6b31452a9e010dd6665dd36f8c96659810d4e1
czp <celenzo.peuch@epitech.eu>
2026-04-07 03:52:14 +0200

    add src/utils/strings/my_strstr.c

---
+++ src/utils/strings/my_strstr.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2026
+** strstr
+** File description:
+** str
+*/
+
+#include "minishell.h"
+
+const char *my_strstr(const char *str, const char *to_f)
+{
+    int i = 0;
+    int k = 0;
+
+    if (*to_f == '\0')
+        return str;
+    while (str[i] != '\0') {
+        k = 0;
+        while (str[i + k] != '\0' && to_f[k] != '\0' && str[i + k] == to_f[k]) {
+            k++;
+        }
+        if (to_f[k] == '\0')
+            return &str[i];
+        i++;
+    }
+    return NULL;
+}
