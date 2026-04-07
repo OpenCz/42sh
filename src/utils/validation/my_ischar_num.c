81e43a38fd613d34cc4ea3272c217b92e2b9816b
czp <celenzo.peuch@epitech.eu>
2026-04-07 03:52:14 +0200

    add src/utils/validation/my_ischar_num.c

---
+++ src/utils/validation/my_ischar_num.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2026
+** my_ischar
+** File description:
+** num
+*/
+
+#include "minishell.h"
+
+int my_char_isnum(char *str)
+{
+    int i = 0;
+
+    if (str[i] >= 48 && str[i] <= 57)
+        return 1;
+    return 0;
+}
