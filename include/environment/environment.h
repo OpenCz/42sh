1c02dac425accb82a91f304b75bed17e9d05cd91
czp <celenzo.peuch@epitech.eu>
2026-04-07 03:52:13 +0200

    add include/environment/environment.h

---
+++ include/environment/environment.h
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2026
+** minishell
+** File description:
+** environment api
+*/
+
+#ifndef ENVIRONMENT_H
+    #define ENVIRONMENT_H
+
+    #include "core/types.h"
+
+char *get_path(env_t *stock_env);
+char *get_home(env_t *stock_env);
+env_t *find_env_node(env_t *env, char *key, env_t **prev);
+char *loop_bin(main_t *main_stock, char *command);
+char *check_bin(char *command, char *path);
+
+#endif
