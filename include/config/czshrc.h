d015e3db62eeb1c1b5277972a39224451e950c3a
ErwanTheGoat <erwan.lo-presti@epitech.eu>
2026-04-09 01:54:34 +0200

    feat: add structures for prompt customization including git branch, folder, user, date, and time

--- include/config/czshrc.h
+++ include/config/czshrc.h
@@ -7,5 +7,22 @@
 
 #ifndef __CZ_SHRT_H__
     #define __CZ_SHRT_H__
+#include <stdbool.h>
+
+typedef struct infos_s
+{
+    bool toggle;
+    int color;
+    int b_color;
+} infos_t;
+
+typedef struct prompt_s
+{
+    infos_t git_branch;
+    infos_t folder;
+    infos_t user;
+    infos_t date;
+    infos_t time;
+} prompt_t;
 
 #endif
