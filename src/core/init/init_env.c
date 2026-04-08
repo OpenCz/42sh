5279ee2445775125e4f648f37b9c4043caa329cd
lukas-sgx <lukas.soigneux@epitech.eu>
2026-04-08 10:14:42 +0200

    fix: memory leak delete

--- src/core/init/init_env.c
+++ src/core/init/init_env.c
@@ -7,21 +7,60 @@
 
 #include "minishell.h"
 
+static char *dup_n(char *src, int n)
+{
+    char *copy = malloc(sizeof(char) * (n + 1));
+
+    if (!copy)
+        return NULL;
+    for (int i = 0; i < n; i++)
+        copy[i] = src[i];
+    copy[n] = '\0';
+    return copy;
+}
+
+static int set_env_value(env_t *node, char *env_string, int sep)
+{
+    if (env_string[sep] != '=') {
+        node->value = NULL;
+        return 0;
+    }
+    node->value = my_strdup(env_string + sep + 1);
+    return node->value == NULL;
+}
+
+static env_t *alloc_env_node(char *env_string, int sep)
+{
+    env_t *node = malloc(sizeof(env_t));
+
+    if (!node)
+        return NULL;
+    node->key = dup_n(env_string, sep);
+    if (!node->key) {
+        free(node);
+        return NULL;
+    }
+    return node;
+}
+
 static env_t *create_env_node(char *env_string)
 {
     env_t *node;
-    char **stock;
+    int sep = 0;
 
-    stock = my_str_to_word_array(env_string, "=");
-    if (!stock)
+    if (!env_string)
         return NULL;
-    node = malloc(sizeof(env_t));
+    while (env_string[sep] && env_string[sep] != '=')
+        sep++;
+    node = alloc_env_node(env_string, sep);
     if (!node)
         return NULL;
-    node->key = stock[0];
-    node->value = stock[1] ? stock[1] : NULL;
+    if (set_env_value(node, env_string, sep)) {
+        free(node->key);
+        free(node);
+        return NULL;
+    }
     node->next = NULL;
-    free(stock);
     return node;
 }
 
