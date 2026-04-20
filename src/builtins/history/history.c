4b22fb55e0f3319faa656ee218896b2c4745810e
Jessymgadd <jessym1.gaddacha@epitech.eu>
2026-04-20 21:45:22 +0200

    feat: add reverse history option

--- src/builtins/history/history.c
+++ src/builtins/history/history.c
@@ -23,18 +23,29 @@ void write_limited_history(char *file, command_ctx_t *ctx)
         printf("%s\n", arr[i]);
         tmp++;
     }
+    free_array(arr);
+}
+
+static void history_reverse(char *file, command_ctx_t *ctx)
+{
+    int n = atoi(ctx->argv[2]);
+    char *str = strtok(file, "\n");
+
+    for (int i = 0; i < n && str; i++) {
+        printf("%s\n", str);
+        str = strtok(NULL, "\n");
+    }
 }
 
 int builtin_history(main_t *main_stock, command_ctx_t *ctx)
 {
     char *file = openator(".c_zsh_history");
-    int len = my_wordarray_len(ctx->argv);
 
     if (!file)
         return 1;
-    if (len == 1)
-        printf("%s", file);
-    else
+    if (ctx->argv[1] && strcmp(ctx->argv[1], "-r") == 0) {
+        history_reverse(file, ctx);
+    } else
         write_limited_history(file, ctx);
     free(file);
     return 0;
