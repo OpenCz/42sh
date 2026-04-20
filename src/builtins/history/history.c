8cc8255a28e66d1ae63c8f5f33de5c41fe103500
Jessymgadd <jessym1.gaddacha@epitech.eu>
2026-04-20 21:06:15 +0200

    feat: limited history print

--- src/builtins/history/history.c
+++ src/builtins/history/history.c
@@ -7,13 +7,28 @@
 
 #include "c_zsh.h"
 
+void write_limited_history(char *file, command_ctx_t *ctx)
+{
+    int n = atoi(ctx->argv[1]);
+    char *str = strtok(file, "\n");
+
+    for (int i = 0; i < n && str != NULL; i++) {
+        printf("%s\n", str);
+        str = strtok(NULL, "\n");
+    }
+}
+
 int builtin_history(main_t *main_stock, command_ctx_t *ctx)
 {
     char *file = openator(".c_zsh_history");
+    int len = my_wordarray_len(ctx->argv);
 
     if (!file)
         return 1;
-    printf("%s", file);
+    if (len == 1)
+        printf("%s", file);
+    else
+        write_limited_history(file, ctx);
     free(file);
     return 0;
 }
