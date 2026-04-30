ddb48397afe65c73aa3d091f582a4947ce961b94
lukas-sgx <lukas.soigneux@epitech.eu>
2026-05-01 00:42:12 +0200

    fix: resolve multiples memory leaks

--- src/parsing/env_var_management/replace_env_vars.c
+++ src/parsing/env_var_management/replace_env_vars.c
@@ -54,10 +54,16 @@ static char *manage_substitution(main_t *stock_main, const char *key,
     int key_len)
 {
     char *command = NULL;
+    char *result = NULL;
+
 
     command = strdup(key);
+    if (!command)
+        return NULL;
     command[key_len] = '\0';
-    return command_substitution(stock_main, command);
+    result = command_substitution(stock_main, command);
+    free_alloc(command);
+    return result;
 }
 
 static char *find_value(char *key, main_t *stock_main)
@@ -84,26 +90,36 @@ static char *find_value(char *key, main_t *stock_main)
     return NULL;
 }
 
+static int undefined_var(char *env_var, char **args, int i)
+{
+    if (!env_var) {
+        my_putstr(args[i] + 1);
+        free_alloc(args[i]);
+        args[i] = my_strdup(": Undefined variable.");
+    }
+    return 0;
+}
+
 static int replace_single_arg(char **args, int i, main_t *stock_main)
 {
     char *env_var = NULL;
     char *suffix = NULL;
     char *new_val = NULL;
+    int is_subst = 0;
 
     if (args[i][0] != '$')
         return 0;
+    if (args[i][1] == '(' || (args[i][1] == '{' && args[i][2] == '('))
+        is_subst = 1;
     env_var = find_value(args[i], stock_main);
-    if (!env_var) {
-        my_putstr(args[i] + 1);
-        free_alloc(args[i]);
-        args[i] = my_strdup(": Undefined variable.");
-        return 0;
-    }
+    undefined_var(env_var, args, i);
     suffix = args[i] + 1 + get_var_name_len(args[i] + 1)
         + (args[i][1] == '{' || args[i][1] == '(' ? 1 : 0);
     new_val = my_strconcat(env_var, suffix);
     free_alloc(args[i]);
     args[i] = new_val;
+    if (is_subst)
+        free_alloc(env_var);
     return 0;
 }
 
