88c94b14aac78c8caffbfe79865a27591c09114f
lukas-sgx <lukas.soigneux@epitech.eu>
2026-04-30 16:56:37 +0200

    feat: implement foreground builtin

--- src/builtins/jobs/my_foreground.c
+++ src/builtins/jobs/my_foreground.c
@@ -9,5 +9,23 @@
 
 int builtin_foreground(main_t *main_stock, command_ctx_t *ctx)
 {
+    job_controler_t *curr = main_stock->controler;
+    job_controler_t *prev = curr;
+    job_controler_t *tmp = curr ? curr->next : NULL;
+
+    if (tmp == NULL) {
+        printf("fg: No current job.\n");
+        return 0;
+    }
+    while (tmp->next != NULL) {
+        prev = tmp;
+        tmp = tmp->next;
+    }
+    kill(tmp->job->pid, SIGCONT);
+    prev->next = tmp->next;
+    free_array(tmp->job->command);
+    free_alloc(tmp->job);
+    free_alloc(tmp);
+    tmp = NULL;
     return 0;
 }
