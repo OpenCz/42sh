037ec42e5712d71643a86b07984135b331a1c59b
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-02 17:31:07 +0200

    feat: refactor backtick processing functions for improved memory management and clarity

--- src/parsing/manage_backticks/manage_backticks.c
+++ src/parsing/manage_backticks/manage_backticks.c
@@ -16,12 +16,11 @@ static char *append_str(char *result, const char *src, int len)
     char *new_result = malloc(rlen + len + 1);
 
     if (!new_result)
-        return result;
+        return NULL;
     if (result)
         memcpy(new_result, result, rlen);
     memcpy(new_result + rlen, src, len);
     new_result[rlen + len] = '\0';
-    free(result);
     return new_result;
 }
 
@@ -33,23 +32,49 @@ static void update_quote_state(char c, int *in_single, int *in_double)
         *in_double = !*in_double;
 }
 
-static char *do_subst(char *inner_cmd, int len, main_t *stock_main)
-{
-    char *inner = strndup(inner_cmd, len);
-    char *subst = inner ? command_substitution(stock_main, inner) : NULL;
-
-    free(inner);
-    if (!subst)
-        subst = my_strdup("");
-    return subst;
-}
-
 static char *do_subst_and_update(char *cmd, int i, int *bts, main_t *sm)
 {
-    char *s = do_subst(cmd + bts[0], i - bts[0], sm);
+    char *inner = strndup(cmd + bts[0], i - bts[0]);
+    char *s = inner ? command_substitution(sm, inner) : NULL;
 
+    free(inner);
+    if (!s)
+        s = my_strdup("");
     bts[1] = i + 1;
     bts[0] = 0;
+    return s;
+}
+
+static char *handle_opening_backtick(char *cmd, char *result,
+    int i, int *bts)
+{
+    char *s = append_str(result, cmd + bts[1], i - bts[1]);
+
+    free(result);
+    if (!s)
+        return NULL;
+    bts[1] = i;
+    bts[0] = i + 1;
+    return s;
+}
+
+static char *do_append_subst(char *result, char *subst)
+{
+    char *new = NULL;
+
+    if (!subst)
+        return NULL;
+    new = append_str(result, subst, strlen(subst));
+    free(subst);
+    free(result);
+    return new;
+}
+
+static char *append_tail(char *cmd, char *result, int bts1)
+{
+    char *s = append_str(result, cmd + bts1, strlen(cmd + bts1));
+
+    free(result);
     return s;
 }
 
@@ -59,23 +84,21 @@ static char *process_backtick_loop(char *cmd, char *result,
     int in_single = 0;
     int in_double = 0;
     int bts[2] = {0, 0};
-    char *s = NULL;
 
     for (int i = 0; cmd[i]; i++) {
         if (!bts[0])
             update_quote_state(cmd[i], &in_single, &in_double);
         if (cmd[i] != '`' || in_single || in_double)
             continue;
-        if (!bts[0]) {
-            result = append_str(result, cmd + bts[1], i - bts[1]);
-            bts[0] = i + 1;
-        } else {
-            s = do_subst_and_update(cmd, i, bts, stock_main);
-            result = append_str(result, s, strlen(s));
-            free(s);
-        }
+        if (!bts[0])
+            result = handle_opening_backtick(cmd, result, i, bts);
+        else
+            result = do_append_subst(result,
+                do_subst_and_update(cmd, i, bts, stock_main));
+        if (!result)
+            return NULL;
     }
-    return append_str(result, cmd + bts[1], strlen(cmd + bts[1]));
+    return append_tail(cmd, result, bts[1]);
 }
 
 char *manage_backticks(char *cmd, main_t *stock_main)
