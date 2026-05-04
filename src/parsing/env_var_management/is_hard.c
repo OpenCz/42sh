ff83b3d7a47034bae6080ff10569c9d31f9f6b08
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-04 09:58:55 +0200

    feat: update include path and enhance is_hard function with last PID retrieval

--- src/parsing/env_var_management/is_hard.c
+++ src/parsing/env_var_management/is_hard.c
@@ -6,7 +6,7 @@
 ** Authors: @Celz-Pch @Lukas-sgx @ErwanTheKing @sacha-lma @Jessymgadd
 */
 
-#include "c_zsh.h"
+#include "../../../include/c_zsh.h"
 
 static char *get_result(char **hard_keys, char **hard_values, const char *key,
     size_t key_len)
@@ -23,14 +23,23 @@ static char *get_result(char **hard_keys, char **hard_values, const char *key,
     return result;
 }
 
+static char *get_last_pid(main_t *stock_main)
+{
+    job_controler_t *ctrl = stock_main->controler;
+
+    for (ctrl = ctrl->next; ctrl && ctrl->next; ctrl = ctrl->next);
+    return my_itoa(ctrl->job->pid);
+}
+
 char *is_hard(const char *key, size_t key_len, main_t *stock_main)
 {
     char *pid = NULL;
     char *hard_keys[] = {"0", "36", "117 354 889 550", "69", "8",
-        "12", "?", "$", NULL};
+        "12", "?", "$", "!", NULL};
     char *hard_values[] = {"c_zsh", "Sasha Le Moins-Avalos",
         "Celenzo Peuch", "Lukas Soigneux", "Jessym Gaddacha",
-        "Erwan Lo Presti", stock_main ? stock_main->last_exit : "0", NULL};
+        "Erwan Lo Presti", stock_main ? stock_main->last_exit : "0",
+        my_itoa(getpid()), get_last_pid(stock_main), NULL};
     char *result = NULL;
 
     if (!stock_main)
