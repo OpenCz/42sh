3fe435954e3e90fa34738e2da3bbd19e6e67e4cf
lukas-sgx <lukas.soigneux@epitech.eu>
2026-05-05 01:08:13 +0200

    fix: optimization of for -> execute _builtin function

--- src/execution/dispatch/execute_builtin.c
+++ src/execution/dispatch/execute_builtin.c
@@ -39,9 +39,8 @@ int execute_builtin(main_t *main_stock, command_ctx_t *ctx)
 {
     char *cmd = ctx->command;
 
-    for (size_t i = 0; command_shell[i].name != NULL; i++) {
+    for (size_t i = 0; command_shell[i].name != NULL; i++)
         if (my_strcmp(cmd, (char *)command_shell[i].name) == 0)
             return command_shell[i].func(main_stock, ctx);
-    }
     return -1;
 }
