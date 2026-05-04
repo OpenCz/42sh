bb13db7ca10e26fdae9c7cadf86a0473ecc7ca14
Celz-Pch <celenzo.peuch@epitech.eu>
2026-05-04 23:17:39 +0200

    feat: alais start parsing for tcsh alias

--- src/builtins/fs/my_alias.c
+++ src/builtins/fs/my_alias.c
@@ -27,17 +27,12 @@ char *my_strdup_quote(char *str)
 int builtin_alias(main_t *main_stock, command_ctx_t *ctx)
 {
     alias_stock_t *new_alias = malloc(sizeof(alias_stock_t));
-    char **arg_alias = NULL;
 
-    if (!new_alias || !ctx->arg_command[0])
-        return 1;
-    arg_alias = my_str_to_word_array(ctx->arg_command[0], "=");
-    if (!arg_alias || !arg_alias[0] || !arg_alias[1])
-        return 1;
-    new_alias->new_name = my_strdup(arg_alias[0]);
-    new_alias->command = my_strdup(arg_alias[1]);
-    new_alias->is_fixed = false;
-    new_alias->next = main_stock->alias_stock;
-    main_stock->alias_stock = new_alias;
+    if (!new_alias)
+        return 84;
+    if (!ctx->arg_command[0] && !ctx->arg_command[1])
+        return 84;
+    new_alias->command = strdup(ctx->arg_command[0]);
+    new_alias->new_name = strdup(ctx->arg_command[1]);
     return 0;
 }
