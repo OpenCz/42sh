eef9df5f68c3d6df3dd5ee342d727ebfda5164ec
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-05 23:42:32 +0200

    feat: add loop function to display ASCII title from file

---
+++ instaler/src/core/loop/loop.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2026
+** 42SH
+** File description:
+** loop
+*/
+
+#include "../../../include/instaler.h"
+
+void loop(int c)
+{
+    char *title_str = openator("assets/title.txt");
+    char **title_arr = title_str ?
+        my_str_to_word_array(title_str, "\n") : NULL;
+
+    while (c != 'q') {
+        clear();
+        print_ascii(1, 15, title_arr);
+        refresh();
+        c = getch();
+    }
+}
