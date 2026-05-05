51ab90eb5a80bf5cc08886f0652c43364442839a
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-05 23:40:32 +0200

    feat: include additional headers for core and display functionalities in installer

--- instaler/include/instaler.h
+++ instaler/include/instaler.h
@@ -5,9 +5,14 @@
 ** instaler
 */
 #include <ncurses.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #ifndef INSTALER_H
     #define INSTALER_H
     #include "utils/utils.h"
     #include "init/init.h"
+    #include "core/core.h"
+    #include "display/display.h"
 #endif
