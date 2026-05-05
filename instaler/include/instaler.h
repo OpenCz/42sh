09a4b467f83ae5fef39626d5dafc8f0ebaae7672
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-05 18:08:29 +0200

    feat: add header file for installer with ncurses support

---
+++ instaler/include/instaler.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2026
+** 42SH
+** File description:
+** instaler
+*/
+#include <ncurses.h>
+
+#ifndef INSTALER_H
+    #define INSTALER_H
+    #include "utils/utils.h"
+    #include "init/init.h"
+#endif
