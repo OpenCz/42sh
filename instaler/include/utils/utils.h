f6b5f61a17ac1ab3b25817c98a14791f8b6e9147
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-05 23:40:49 +0200

    feat: add utility functions for file handling and string manipulation

--- instaler/include/utils/utils.h
+++ instaler/include/utils/utils.h
@@ -7,5 +7,11 @@
 
 #ifndef UTILS_H
     #define UTILS_H
-
+char *openator(char const *filepath);
+void free_alloc(void *object);
+void free_array(char **array);
+int get_file_size_stat(char const *filepath);
+char **my_str_to_word_array(char *str, char *separator);
+char ***my_str_to_array_of_word_array(char const *str, char const *sep_ext,
+    char const *sep_int);
 #endif
