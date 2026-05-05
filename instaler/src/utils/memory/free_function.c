13388ca373511d9a9c92c6e21240d2dcee67b889
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-05 23:41:07 +0200

    feat: implement memory cleanup functions for safe deallocation

---
+++ instaler/src/utils/memory/free_function.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2026
+** 42sh
+** File description:
+** Full memory cleanup: free_alloc (NULL-safe free), free_array
+** (char** + each element), free_linked_list (env_t chain),
+** free_history (doubly-linked list), free_main (full teardown).
+** Authors: @Celz-Pch @Lukas-sgx @ErwanTheKing @sacha-lma @Jessymgadd
+*/
+
+#include "../../../include/instaler.h"
+
+void free_alloc(void *object)
+{
+    if (object)
+        free(object);
+    object = NULL;
+}
+
+void free_array(char **array)
+{
+    if (!array)
+        return;
+    for (int i = 0; array[i] != NULL; i++)
+        free_alloc(array[i]);
+    free_alloc(array);
+}
