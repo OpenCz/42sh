facb132f758e670eb51beb15af80a56ec45d4b1c
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-06 03:04:05 +0200

    feat: extend loop function and add installer structure for window management

--- instaler/include/core/core.h
+++ instaler/include/core/core.h
@@ -8,5 +8,13 @@
 #ifndef CORE_H
     #define CORE_H
     #include "../instaler.h"
-void loop(int c);
+typedef struct win_size_s {
+    int x;
+    int y;
+}win_size_t;
+typedef struct instaler_s {
+    win_size_t size;
+    WINDOW *window;
+}instaler_t;
+void loop(int c, instaler_t *instaler);
 #endif
