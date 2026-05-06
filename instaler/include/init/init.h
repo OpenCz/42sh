e9563fa050967472b20c7192b3025080951ccbf1
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-06 03:04:14 +0200

    feat: update init function to accept instaler_t structure and add closup function

--- instaler/include/init/init.h
+++ instaler/include/init/init.h
@@ -5,10 +5,9 @@
 ** init
 */
 
-#include <ncurses.h>
-
-
 #ifndef INIT_H
     #define INIT_H
-void init(WINDOW *window);
+    #include "../core/core.h"
+void init(instaler_t *instaler);
+void closup(instaler_t *instaler);
 #endif
