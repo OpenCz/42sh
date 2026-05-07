6e2d7bcd4842d59baecbfdfbe08b53b9069b3f2e
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-07 17:24:18 +0200

    feat: update main function to streamline loop execution and cleanup process

--- instaler/src/core/main.c
+++ instaler/src/core/main.c
@@ -10,10 +10,10 @@
 int main(void)
 {
     instaler_t *instaler = malloc(sizeof(instaler_t));
-    int c = 0;
 
     instaler->window = initscr();
     init(instaler);
-    loop(c, instaler);
-    endwin();
+    loop(instaler);
+    closup(instaler);
+    return 0;
 }
