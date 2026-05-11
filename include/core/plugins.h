710773182d192339424202b9219c163030e4d345
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-11 14:51:30 +0200

    style: format code for improved readability in plugins.h

--- include/core/plugins.h
+++ include/core/plugins.h
@@ -12,25 +12,25 @@
     #define CZSH_PLUGIN_OK 0
     #define CZSH_PLUGIN_ERR -1
 
-    typedef struct main_s main_t;
-    typedef struct command_ctx_s command_ctx_t;
+typedef struct main_s main_t;
+typedef struct command_ctx_s command_ctx_t;
 
-    typedef int (*builtin_func_t)(main_t *main_stock,
-        command_ctx_t *ctx);
+typedef int (*builtin_func_t)(main_t *main_stock,
+    command_ctx_t *ctx);
 
-    typedef int (*register_builtin_fn_t)(main_t *main_stock,
-        const char *name, builtin_func_t func, void *plugin);
+typedef int (*register_builtin_fn_t)(main_t *main_stock,
+    const char *name, builtin_func_t func, void *plugin);
 
-    typedef struct shell_plugin_api_s {
-        int abi_version;
-        register_builtin_fn_t register_builtin;
-        void *plugin_handle;
-    } shell_plugin_api_t;
+typedef struct shell_plugin_api_s {
+    int abi_version;
+    register_builtin_fn_t register_builtin;
+    void *plugin_handle;
+} shell_plugin_api_t;
 
-    typedef int (*plugin_init_fn_t)(main_t *main_stock,
-        const shell_plugin_api_t *api);
+typedef int (*plugin_init_fn_t)(main_t *main_stock,
+    const shell_plugin_api_t *api);
 
-    int czsh_plugin_init(main_t *main_stock, const shell_plugin_api_t *api);
-    void czsh_plugin_shutdown(main_t *main_stock);
+int czsh_plugin_init(main_t *main_stock, const shell_plugin_api_t *api);
+void czsh_plugin_shutdown(main_t *main_stock);
 
 #endif
